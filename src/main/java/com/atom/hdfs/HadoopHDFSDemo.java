e1703d42742df1dede01a1daa913e42358ae272b
Atom <sarming@126.com>
2023-07-11 17:43:20 +0800

    hdfs api

--- src/main/java/com/atom/hdfs/HadoopHDFSDemo.java
+++ src/main/java/com/atom/hdfs/HadoopHDFSDemo.java
@@ -2,7 +2,9 @@ package com.atom.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
 import org.apache.hadoop.fs.permission.FsAction;
 import org.apache.hadoop.fs.permission.FsPermission;
 import org.slf4j.Logger;
@@ -31,7 +33,7 @@ public class HadoopHDFSDemo {
         configuration.set("dfs.replication", "1");
         configuration.set("dfs.client.use.datanode.hostname", "true");
 // 设置客户端身份为管理员 root ,(root也可能不是管理员，这时就需要设置具体的管理员用户)
-//        System.setProperty("HADOOP__USER_NAME", "root");
+        System.setProperty("HADOOP__USER_NAME", "root");
         fileSystem = FileSystem.get(new URI(HDFS_PATH), configuration, HDFS_USER);
 
         if (createDirectory()) {
@@ -42,19 +44,32 @@ public class HadoopHDFSDemo {
             LOGGER.info("upload file success.");
         }
 
+        listFiles();
+
+//
+
+    }
+
+    private static void listFiles() throws IOException {
+
+        RemoteIterator<LocatedFileStatus> locatedFileStatusRemoteIterator = fileSystem.listFiles(new Path("/test002"), false);
+        while (locatedFileStatusRemoteIterator.hasNext()){
+            LocatedFileStatus next = locatedFileStatusRemoteIterator.next();
+            System.err.println(next.getPath().getName());
+        }
     }
 
 
     public static boolean createDirectory() throws IOException {
-        return fileSystem.mkdirs(new Path("/resources/test002/"),
+        return fileSystem.mkdirs(new Path("/test002/"),
                 new FsPermission(FsAction.ALL, FsAction.ALL, FsAction.ALL));
     }
 
 
     public static boolean uploadLocalFile() throws IOException {
-        Path src = new Path("/Users/atom/jxplorerjxconfig.txt");
+        Path src = new Path("/Users/atom/hello.txt");
         // hdfs 会自动创建父文件夹
-        Path dest = new Path("/dir2/12.txt");
+        Path dest = new Path("/test002/12.txt");
 
         //还可以设置是否删除本地文件，以及是否覆盖hdfs中的原有同名文件
         //默认不删除本地文件，覆盖远端文件
@@ -64,6 +79,7 @@ public class HadoopHDFSDemo {
             LOGGER.error("hdfs upload local file fail ", e);
             return false;
         }
+        LOGGER.info("hdfs upload local file [{}] success ", src.getName());
 //        fileSystem.copyFromLocalFile(true, src, dest);
 //        fileSystem.copyFromLocalFile(true, true, src, dest);
         return true;
